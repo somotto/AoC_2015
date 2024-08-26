6cf48b68e284ab9fc9c9187aae2fea89e65ef5a0
somotto <stephenonyango4783@gmail.com>
2024-08-27 00:33:09 +0300

    style: format the code

--- day_1/Not_quite_lisp/position.go
+++ day_1/Not_quite_lisp/position.go
@@ -1,16 +1,16 @@
 package Not_quite_lisp
 
-func Position(input string) int{
+func Position(input string) int {
 	floor := 0
 
-	for i, step := range input{
+	for i, step := range input {
 		if step == '(' {
 			floor++
 		} else {
 			floor--
 		}
 		if floor == -1 {
-			return i +1
+			return i + 1
 		}
 	}
 	return -1
