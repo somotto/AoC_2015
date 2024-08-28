5f9e4a868ab894cfc2feec0e6595e5c278ec6bb0
somotto <stephenonyango4783@gmail.com>
2024-08-28 22:18:46 +0300

    test: add test file

---
+++ day_3/houses/counthouses_test.go
@@ -0,0 +1,25 @@
+package houses
+
+import (
+	"testing"
+)
+
+func TestCountHouses(t *testing.T) {
+	tests := []struct {
+		directions string 
+		want       int    
+	}{
+		{">", 2},          
+		{"^>v<", 4},        
+		{"^v^v^v^v^v", 2},  
+		{"", 1},             
+		{">>^^<<vv", 8},  
+	}
+
+	for _, tt := range tests {
+		got := CountHouses(tt.directions)
+		if got != tt.want {
+			t.Errorf("CountHouses(%q) got %d, want %d", tt.directions, got, tt.want)
+		}
+	}
+}
