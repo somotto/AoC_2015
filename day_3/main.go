bdbb93ff35f17c499dabeb7d5fe9e020ea4b808d
somotto <stephenonyango4783@gmail.com>
2024-08-28 21:47:18 +0300

    feat: initial main function

---
+++ day_3/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"day_3/houses"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 1 {
+		return
+	}
+
+	data, err := os.ReadFile("direction.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	directions := string(data)
+	housesRecievedMore := houses.CountHouses(directions)
+
+	fmt.Println(housesRecievedMore)
+
+}
