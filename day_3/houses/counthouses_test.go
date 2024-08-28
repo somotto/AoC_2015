3d59c7129fea3b5c1e0f4b0dd0d96c2e5123ceb4
somotto <stephenonyango4783@gmail.com>
2024-08-29 00:12:39 +0300

    style:format code

--- day_3/houses/counthouses_test.go
+++ day_3/houses/counthouses_test.go
@@ -6,14 +6,14 @@ import (
 
 func TestCountHouses(t *testing.T) {
 	tests := []struct {
-		directions string 
-		want       int    
+		directions string
+		want       int
 	}{
-		{">", 2},          
-		{"^>v<", 4},        
-		{"^v^v^v^v^v", 2},  
-		{"", 1},             
-		{">>^^<<vv", 8},  
+		{">", 2},
+		{"^>v<", 4},
+		{"^v^v^v^v^v", 2},
+		{"", 1},
+		{">>^^<<vv", 8},
 	}
 
 	for _, tt := range tests {
