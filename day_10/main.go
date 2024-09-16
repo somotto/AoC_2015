418d72fe9a458a970a742f115103d4b219d1b3e1
somotto <stephenonyango4783@gmail.com>
2024-09-16 17:23:08 +0300

    print part2

--- day_10/main.go
+++ day_10/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"day_10/part1"
+	"day_10/part2"
 	"fmt"
 )
 func main() {
 	inputSequence := "3113322113"
 	length := part1.LengthOf40thLookAndSay(inputSequence)
+	length2 := part2.LengthOf50thLookAndSay(inputSequence)
+
+
 	fmt.Println(length)
+	fmt.Println(length2)
 }
